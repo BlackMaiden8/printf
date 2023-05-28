706c216b4defcf5773433a4b76df217a619a15f1
BlackMaiden8 <frimpongednita@gmail.com>
2023-05-28 13:34:26 -0700

    first function

--- main.h
+++ main.h
@@ -46,11 +46,11 @@ va_list pad, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
+int print_char(va_list diff, char buffer[],
 	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
+int print_string(va_list diff, char buffer[],
 	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
+int print_percent(va_list diff, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
